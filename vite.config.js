20146d2b4868bd365206d7a6ca9ddde405593b4b
Ayush Gawali <ayushgawali.12345@gmail.com>
2025-06-21 21:29:14 +0530

    vite.config.js modified

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/ecom-react-vite/', // Replace with your actual repo name
+  base: '/React-Pagination/', // Replace with your actual repo name
 })
